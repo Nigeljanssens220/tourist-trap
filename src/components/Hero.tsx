ed02a33c48b15bf735b1e675010b892d10d25fce
Nigel Janssens <janssensnigel@gmail.com>
2022-02-24 16:11:59 +0100

    cleanup and debug

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ type Props = {};
 const Hero: FC = (props: Props) => {
   return (
     <div className="bg-white dark:bg-gray-800 overflow-hidden relative lg:flex lg:items-center max-w-screen-2xl mx-auto">
-      <div className="w-full py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+      <div className="py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
         <h2 className="text-center md:text-left text-3xl font-semibold text-black dark:text-white sm:text-4xl">
           <span className="block">Mother hearth host your travel</span>
         </h2>
