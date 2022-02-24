d9bc92ee517e298c80d5d6f0f9af170fcf1d7791
Nigel Janssens <janssensnigel@gmail.com>
2022-02-24 16:18:20 +0100

    delete redundant files

--- src/components/AddButton.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
-
-function AddButton() {
-  return <AddCircleOutlineOutlinedIcon className='text-blue-900' />;
-}
-
-export default AddButton;
--- src/components/old/SubtractButton.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import RemoveCircleOutlineOutlinedIcon from "@mui/icons-material/RemoveCircleOutlineOutlined";
-
-function SubtractButton() {
-  return <RemoveCircleOutlineOutlinedIcon className='text-blue-900' />;
-}
-
-export default SubtractButton;
