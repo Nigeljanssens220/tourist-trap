3441152de5cc1a1592b555f4edf9dd05100a2034
Nigel Janssens <janssensnigel@gmail.com>
2022-03-25 14:53:40 +0100

    fix hero

--- src/components/Hero/hero2.tsx
+++ src/components/Hero/hero2.tsx
@@ -1,17 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const Hero2 = () => {
   return (
     <div className="relative w-full max-w-screen-2xl ">
       <div className="absolute inset-x-0 bottom-0 h-1/2 " />
-      <div className="mx-auto sm:px-6 lg:px-8 ">
+      <div className="mx-auto md:px-6 lg:px-8 ">
         <div className="relative  sm:rounded-2xl ">
           <div className="absolute inset-0">
-            <img
-              className="h-full w-full object-cover "
+            <Image
               src="/mountains-grayscale.jpg"
-              //   src='https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100'
-              alt="Mountain Range"
+              alt="Hero"
+              objectFit="cover"
+              layout="fill"
             />
             <div className="absolute inset-0 " />
           </div>
